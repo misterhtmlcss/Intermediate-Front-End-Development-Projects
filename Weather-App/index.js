11a1184ccc5a15c852d3f8b19eceff3efa499abd
Roger K <misterhtmlcss@gmail.com>
2018-05-10 09:38:45 -0600

    Cleaned my var declarations at the top, broke out a functional script at the bottom that was done inside of convTemp and fixed convTemp so it works. Additional Testing to follow

--- Weather-App/index.js
+++ Weather-App/index.js
@@ -1,61 +1,68 @@
-const cloudIcon = document.getElementById("cloudIcon")
-let data;
-let lat;
-let long;
-let url;
+const cloudIcon = document.getElementById('cloudIcon')
+const cel = document.getElementById('celcius')
+const fah = document.getElementById('fahrenheit')
+const degNum = document.getElementById('degNum')
+const fdeg = document.getElementById('fdeg')
+const cdeg = document.getElementById('cdeg')
 
-const cel = document.getElementById("celcius")
-const fah = document.getElementById("fahrenheit")
-let degNum = document.getElementById("degNum")
-let fdeg = document.getElementById("fdeg")
-let cdeg = document.getElementById("cdeg")
-let cTemp;
-let fTemp;
-let gTemp;
+let data, lat, long, url;
+
+let cTemp, gTemp;
 
 if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-                lat = Math.floor(position.coords.latitude)
-                long = Math.floor(position.coords.longitude)
-                const cUrl = function createUrl (x, y) {
-                        return url = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`
-                }
-                // fetch was here.
-                getWeather(cUrl());
-                })
-        } else {
-                console.log ("looks like something is broke when calling the gelocation method")
-        }
+   navigator.geolocation.getCurrentPosition(position => {
+      lat = Math.floor(position.coords.latitude)
+      long = Math.floor(position.coords.longitude)
+
+      getWeather(cUrl(lat, lomng))
+   })
+} else {
+   console.log("looks like something is broke when calling the gelocation method")
+}
 
 function getWeather(x) {
-        fetch(x)
-                .then( (res) => res.json() )
-                .then( (blob) => {
-                        data = blob
-                        returnData()
-        })
-}
-function returnData() {
-        const { main: {humidity, pressure, temp},
-        wind: {speed}, name} = data // descturing the returned object
-        const {description, icon } = data.weather[0];
-        gTemp = `${Math.floor(temp)}`
-        //degNum.textContent=gTemp
-        //cloudIcon.innerHTML = `<img src="${icon}">`
+   fetch(x)
+      .then((res) => res.json())
+      .then((blob) => {
+         data = blob
+         returnData()
+      })
 }
 
+function returnData() {
+   const {
+      main: {
+         humidity,
+         pressure,
+         temp
+      },
+      wind: {
+         speed
+      },
+      name
+   } = data // destructuring the returned object
+   const {
+      description,
+      icon
+   } = data.weather[0]
+   gTemp = `${Math.floor(temp)}`
+}
+
+
+function toF(cel) {
+   return cel * 9 / 5 + 32
+}
 
 function convTemp(gtemp) {
-        cTemp = gtemp
-        fTemp = gtemp*9/5+32
-        if(degNum === cTemp) {
-                return degNum.textContent=cTemp
-        } else {
-                return degNum.textContent=fTemp
-        }
-        appendClass();
+   const fTemp = toF(gTemp)
+   if (degNum === gTemps) {
+      degNum.textContent = gtemp
+   } else {
+      degNum.textContent = fTemp
+   }
+appendClass()
 }
-function appendClass () {
-        fdeg.classList.toggle("hide");
-        cdeg.classList.toggle("hide");
+function appendClass() {
+   fdeg.classList.toggle('hide')
+   cdeg.classList.toggle('hide')
 }
