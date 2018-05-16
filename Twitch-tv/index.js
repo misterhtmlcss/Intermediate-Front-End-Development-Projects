7c622f16dcdfb37dd05777405aa7fc34fc656bfb
Roger K <misterhtmlcss@gmail.com>
2018-05-16 10:45:43 -0600

    refactored code using a createContainer function to breakout creatEl.. for data to be displayed

--- Twitch-tv/index.js
+++ Twitch-tv/index.js
@@ -1,52 +1,65 @@
-import fetch from 'node-fetch'
-
 const users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "rpglimitbreak"]
-const views = ["users", "streams", "channels"]
-const app = document.querySelector('#app')
 const http = new XMLHttpRequest()
-const fccClick = document.querySelector('.user-fcc')
-const fcc = "freecodecamp"
-let view, user, url;
+const baseURL = "https://wind-bow.glitch.me/twitch-api/channels/"
+const app = document.querySelector('#app')
+let url, profile
 
+let offline = document.createElement('div')
+let online =  document.createElement('div')
 
-/* fccClick.addEventListener('click', function(e) {
-  e.preventDefault();
-} */
 
 window.addEventListener("load", function(event) {
   console.log("All resources finished loading!");
+  getUserData()
 });
 
-
-function xhr () {
-	http.onreadystatechange = function() {
-		if (this.readyState === 4 && this.status == 200) {
-			const response = JSON.parse(http.responseText)
-			//console.log(response)
-		}
-	}
-	http.open("GET", url, true);
-	http.send();
+function getUserData () {
+  users.forEach(function (user){
+    url = `${baseURL}${user}`
+    //console.log(url)
+    fetch(url)
+      .then((res) => res.json())
+      .then((blob) => {
+        data = blob
+        //console.log(data)
+        const {display_name: username , logo: logoImg, url, status } = data
+        createContainer(username, logoImg, url, status)
+      })
+      .catch(function(error) {
+        console.log(`What went wrong ${error}`);
+      })
+  })
 }
 
-function fccStreaming () {
-	if()
-}
+function createContainer(username, logoImg, url, status) {
+  if (status == undefined || null){
+    offline = offline.setAttribute('class', 'wrapper offline')
+    console.log(offline + " offline") // Getting Undefined
+    profile =
+    `
+    <a href="${url}">
+      <ul class="offline" id="${username}">
+        <li class="logoImg"><img src="${logoImg}" alt="${username}_image"></li>
+        <li class="user">Name: ${username}</li>
+      </ul>
+    </a>
+    `
+    app.appendChild(offline)
+    offline.innerHTML = profile
+  } else {
+      online = online.setAttribute('class', 'wrapper online')
+      console.log(online + " online")  // Getting Undefined
+      profile =
+      `
+      <a href="${url}">
+        <ul class="online" id="${username}">
+          <li class="logoImg"><img src="${logoImg}" alt="${username}_image"></li>
+          <li class="user">Name: ${username}</li>
 
-function getView () {
-
-
-	function getUsers () {
-
-
-		function buildURL () {
-
-
-		}
-	}
-}
-
-
-function getActives () {
-
+        </ul>
+      </a>
+      `
+      app.appendChild(online)
+      online.innerHTML = profile
+  }
 }
